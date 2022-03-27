23281b0306c06e2acebc67f745e82606ab5cc1bc
João Ceia <joao.p.ceia@gmail.com>
2022-03-27 03:01:16 +0100

    setting HttpResponse fields as protected instead of private, to be accessed by parser derived class

--- inc/HTTP/Response.hpp
+++ inc/HTTP/Response.hpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 15:40:09 by jceia             #+#    #+#             */
-/*   Updated: 2022/03/27 00:11:26 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/03/27 02:40:05 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,8 @@ public:
     // IO operators
     friend std::ostream &operator<<(std::ostream &out, const HttpResponse &request);
 
-private:
+protected:
+
     HttpStatus _status;
 };
 
